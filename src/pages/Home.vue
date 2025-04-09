5c27739e7df3de729e2eefc1ecb3f2ed72ee2c32
N1krat <bogdanbraniste2007@gmail.com>
2025-04-09 08:44:36 +0300

    finishing main page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -13,13 +13,13 @@
 
           <div class="colImages">
             <div class="image">
-              <img src="https://placehold.co/500x500" alt="placeholder image">
+              <img src="https://placehold.co/500x500" alt="placeholder image" class="w-full h-full object-cover">
             </div>
           </div>
 
           <div class="colImages">
             <div class="image">
-              <img src="https://placehold.co/500x500" alt="placeholder image">
+              <img src="https://placehold.co/500x500" alt="placeholder image" class="w-full h-full object-cover">
             </div>
           </div>
 
@@ -55,49 +55,129 @@
 
         <div class="newProductsMain">
           
-
-<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="p-8 rounded-t-lg" src="https://flowbite.com/docs/images/products/apple-watch.png" alt="product image" />
-    </a>
-    <div class="px-5 pb-5">
-        <a href="#">
-            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-        </a>
-        <div class="flex items-center mt-2.5 mb-5">
-            <div class="flex items-center space-x-1 rtl:space-x-reverse">
-                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                </svg>
-                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                </svg>
-                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                </svg>
-                <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                </svg>
-                <svg class="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                </svg>
+        <div class="w-full max-w-sm  rounded-lg  dark:border-gray-700">
+            <a href="#">
+                <img class="p-8 rounded-t-lg bg-gray-50" src="https://flowbite.com/docs/images/products/apple-watch.png" alt="product image" />
+            </a>
+            <div class="px-5 pb-5">
+                <a href="#">
+                    <h5 class="text-xl font-semibold tracking-tight text-black dark:text-black">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+                </a>
+                <br>
+                <div class="flex items-center justify-between">
+                    <span class="text-3xl font-bold text-black dark:text-black">$599</span>
+                    <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                </div>
             </div>
-            <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded-sm dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
         </div>
-        <div class="flex items-center justify-between">
-            <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+
+        <div class="w-full max-w-sm  rounded-lg dark:border-gray-700">
+            <a href="#">
+                <img class="p-8 rounded-t-lg bg-gray-50" src="https://flowbite.com/docs/images/products/apple-watch.png" alt="product image" />
+            </a>
+            <div class="px-5 pb-5">
+                <a href="#">
+                    <h5 class="text-xl font-semibold tracking-tight text-black dark:text-black">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+                </a>
+                <br>
+                <div class="flex items-center justify-between">
+                    <span class="text-3xl font-bold text-black dark:text-black">$599</span>
+                    <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
+
+        <div class="w-full max-w-sm  rounded-lg dark:border-gray-700">
+            <a href="#">
+                <img class="p-8 rounded-t-lg bg-gray-50" src="https://flowbite.com/docs/images/products/apple-watch.png" alt="product image" />
+            </a>
+            <div class="px-5 pb-5">
+                <a href="#">
+                    <h5 class="text-xl font-semibold tracking-tight text-black dark:text-black">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+                </a>
+                <br>
+                <div class="flex items-center justify-between">
+                    <span class="text-3xl font-bold text-black dark:text-black">$599</span>
+                    <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                </div>
+            </div>
+        </div>
 
         </div>
 
 
-        <div class="nextProduct">3</div>
+        <div class="mostPopular mt-20 mx-4">
+          <h2 class="text-4xl font-bold">Our Most Popular Products</h2>
+
+          <div class="popularProductsMain">
+          
+          <div class="w-full max-w-sm  rounded-lg  dark:border-gray-700">
+              <a href="#">
+                  <img class="p-8 rounded-t-lg bg-gray-50" src="https://flowbite.com/docs/images/products/apple-watch.png" alt="product image" />
+              </a>
+              <div class="px-5 pb-5">
+                  <a href="#">
+                      <h5 class="text-xl font-semibold tracking-tight text-black dark:text-black">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+                  </a>
+                  <br>
+                  <div class="flex items-center justify-between">
+                      <span class="text-3xl font-bold text-black dark:text-black">$599</span>
+                      <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                  </div>
+              </div>
+          </div>
+  
+          <div class="w-full max-w-sm  rounded-lg dark:border-gray-700">
+              <a href="#">
+                  <img class="p-8 rounded-t-lg bg-gray-50" src="https://flowbite.com/docs/images/products/apple-watch.png" alt="product image" />
+              </a>
+              <div class="px-5 pb-5">
+                  <a href="#">
+                      <h5 class="text-xl font-semibold tracking-tight text-black dark:text-black">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+                  </a>
+                  <br>
+                  <div class="flex items-center justify-between">
+                      <span class="text-3xl font-bold text-black dark:text-black">$599</span>
+                      <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                  </div>
+              </div>
+          </div>
+  
+          <div class="w-full max-w-sm  rounded-lg dark:border-gray-700">
+              <a href="#">
+                  <img class="p-8 rounded-t-lg bg-gray-50" src="https://flowbite.com/docs/images/products/apple-watch.png" alt="product image" />
+              </a>
+              <div class="px-5 pb-5">
+                  <a href="#">
+                      <h5 class="text-xl font-semibold tracking-tight text-black dark:text-black">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+                  </a>
+                  <br>
+                  <div class="flex items-center justify-between">
+                      <span class="text-3xl font-bold text-black dark:text-black">$599</span>
+                      <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+                  </div>
+              </div>
+          </div>
+  
+          </div>
+  
+
+        </div>
     
       </div>
       
+      <footer class="mt-20">
+        
+        <div class="footerDivs p-5">
+            <div class="info text-xl">Info</div>
+            <div class="links">Email</div>
+            <div class="contact text-xl">Contact</div>
+            <div class="logo">
+              <span class="text-xl">Our brand</span>
+            </div>
+        </div>
+    
+      </footer>
 </template> 
 
 <script>
@@ -148,7 +228,6 @@ export default {
 }
 
 .colImages {
-    background-color: blue;
     grid-row: span 3 / span 3;
 }
 
@@ -198,7 +277,10 @@ export default {
 .newProductsMain {
     grid-column: span 4 / span 4;
     grid-row-start: 2;
-    background-color: bisque;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 1vw;
 }
 
 .nextProduct {
@@ -224,14 +306,50 @@ body {
     #444 20px
   );
   background-size: 20px 20px;
+
+  font-family: 'Beatrice Deck Trial', sans-serif;
+  font-weight: normal;
 }
 
 body {
-  background-image: url("public/Figma_51SJBfcZB1.png"); /* Use the uploaded image */
+  background-image: url("/Figma_51SJBfcZB1.png"); /* Use the uploaded image */
   background-size: cover; /* Adjust to fill the entire page */
   background-repeat: repeat; /* Repeat the texture */
   background-position: center;
 }
 
+/* popular products */
+
+/* footer */
+
+footer { 
+  margin-top: 10vh;
+  font-family: 'Beatrice Deck Trial', sans-serif;
+  font-weight: normal;
+  text-align: center; 
+  background-color: beige;
+  width: 100%;
+}
+
+
+.footerDivs {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    gap: 8px;
+}
+    
+
+.links {
+    grid-column-start: 1;
+    grid-row-start: 2;
+}
+
+.contact {
+    grid-column-start: 2;
+    grid-row-start: 1;
+}
+
         
+
 </style>
